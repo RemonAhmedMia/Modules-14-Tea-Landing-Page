e1effd36ab20c316e21672a253afa9c433ab4464
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-21 09:55:15 +0100

    event-one-added

---
+++ event-dom.js
@@ -0,0 +1,8 @@
+function handleOnclick() {
+    console.log("Ami Aclicked");
+}
+
+function handleOnclick() {
+    const changeText = document.getElementById("change-heading");
+    changeText.innerText = "Creative It Onek vlo"
+}
