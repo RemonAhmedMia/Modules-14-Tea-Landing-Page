bf7d341b86e287d54cf007c36669ec661bd28ea2
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-31 09:55:31 +0100

    testimonial-add

--- event-dom.js
+++
@@ -1,8 +0,0 @@
-function handleOnclick() {
-    console.log("Ami Aclicked");
-}
-
-function handleOnclick() {
-    const changeText = document.getElementById("change-heading");
-    changeText.innerText = "Creative It Onek vlo"
-}
